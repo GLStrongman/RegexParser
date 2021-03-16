855899b91b725d9f99aaf2f5fca7d8a9edcf651d
GStrongman <georgia.strongman@gmail.com>
2021-03-16 20:59:44 +1300

    Check whether target exactly matches regex

--- main.rb
+++ main.rb
@@ -9,3 +9,12 @@ targetFilePath = File.open('targets.txt')
 targetFile = File.read(targetFilePath).split("\n")
 puts regexFile.length
 puts targetFile.length
+
+# Check whether target exactly matches regex
+regexFile.each_with_index do |i, n|
+  if regexFile[n] == targetFile[n] then
+    puts "YES: " + regexFile[n] + " with " + targetFile[n]
+  else
+    puts "NO: " + regexFile[n] + " with " + targetFile[n]
+  end
+end
